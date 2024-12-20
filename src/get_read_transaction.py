72af634365a9ee8bf73179591afb7247cc0c1067
Елена Филипьева <olliwija@mail.ru>
2024-12-20 21:10:04 +0300

    Добавлены тесты и дополнен README

--- src/get_read_transaction.py
+++ src/get_read_transaction.py
@@ -1,33 +1,24 @@
-import pandas as pd
 import os
-
 from typing import Any, Dict, List
 
-from pandas import read_csv
-from pandas.core.methods.to_dict import to_dict
+import pandas as pd
 
 
 def get_read_csv(path_file_csv: str) -> List[Dict[str, Any]]:
-    """ Функция чтения CSV-файла """
+    """Функция чтения CSV-файла"""
     try:
-        df_csv = pd.read_csv(path_file_csv, delimiter=';')
+        df_csv = pd.read_csv(path_file_csv, delimiter=";")
         # print(df_csv.shape)
-        # print(df_csv.head())
+        print(df_csv.head())
     except Exception as e:
         print(type(e).__name__)
         return []
-    dict_read_csv = df_csv.to_dict(orient='records')
+    dict_read_csv = df_csv.to_dict(orient="records")
     return dict_read_csv
 
 
-base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-path_file_csv = os.path.join(base_dir, "data", "transactions.csv")
-read_csv_transaction = get_read_csv(path_file_csv)
-# print(read_csv_transaction)
-
-
 def get_read_excel(path_file_excel: str) -> List[Dict[str, Any]]:
-    """ Функция чтения EXCEL-файла """
+    """Функция чтения EXCEL-файла"""
     try:
         df_xls = pd.read_excel(path_file_excel)
         # print(df_xls.shape)
@@ -35,11 +26,14 @@ def get_read_excel(path_file_excel: str) -> List[Dict[str, Any]]:
     except Exception as e:
         print(type(e).__name__)
         return []
-    dict_read_excel = df_xls.to_dict(orient='records')
+    dict_read_excel = df_xls.to_dict(orient="records")
     return dict_read_excel
 
 
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+path_file_csv = os.path.join(base_dir, "data", "transactions.csv")
 path_file_excel = os.path.join(base_dir, "data", "transactions_excel.xlsx")
+read_csv_transaction = get_read_csv(path_file_csv)
 read_excel_transaction = get_read_excel(path_file_excel)
+# print(read_csv_transaction)
 # print(read_excel_transaction)
